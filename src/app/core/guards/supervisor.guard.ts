898eedca7ddabcb490019ecb41b6d977a8d5028f
Bartłomiej <bartlomiej.wydrzycki@onwelo.com>
2023-11-17 19:26:49 +0100

    New guards

---
+++ src/app/core/guards/supervisor.guard.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { map, Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SupervisorGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | boolean
+    | UrlTree
+    | Promise<boolean | UrlTree>
+    | Observable<boolean | UrlTree> {
+    return this.authService.user.pipe(
+      map((user) => {
+        if (!user?.studentNumber) {
+          return true;
+        }
+        return this.router.createUrlTree(['/']);
+      })
+    );
+  }
+}
