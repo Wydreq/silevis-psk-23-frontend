68eaa2754b8c748ca253e3fc1b57d0e89988b89d
Piotr_Kolodziejczyk <piotr.kolodziejczyk@onwelo.com>
2023-11-17 16:15:51 +0100

    Add modules

---
+++ src/app/super-vision/super-vision.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SuperVisionRoutingModule } from './super-vision-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SuperVisionRoutingModule
+  ]
+})
+export class SuperVisionModule { }
